4704a3d3f536e906076fc0acd286e26e07683996
yeyomam <stgotropical@gmail.com>
2020-03-31 17:30:00 -0400

    Estamos creando nuestra web

---
+++ mail.php
@@ -0,0 +1,17 @@
+<?php
+if(isset( $_POST['name']))
+$name = $_POST['name'];
+if(isset( $_POST['email']))
+$email = $_POST['email'];
+if(isset( $_POST['message']))
+$message = $_POST['message'];
+if(isset( $_POST['subject']))
+$subject = $_POST['subject'];
+
+$content="De: $name \n Correo electrónico: $email \n Mensaje: $message";
+$recipient = "stgotropical@gmail.com";
+$mailheader = "De: $email \r\n";
+mail($recipient, $subject, $content, $mailheader) or die("Location:form/error.html");
+header("Location:form/enviado.html");
+//echo "Email sent!";
+?>
